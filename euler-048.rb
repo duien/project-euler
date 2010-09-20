d3c43771827b4f6bdb799d1e64a5b153e5dab19b
Emily Price <price.emily@gmail.com>
2010-09-20 17:10:42 -0400

    Problem 48 done. Another easy one.

---
+++ euler-048.rb
@@ -0,0 +1,5 @@
+# The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+# 
+# Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+puts (1..1000).inject{ |s, i| s += i**i } % 10000000000
